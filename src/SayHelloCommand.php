43e7514520f333fab0b9e61d325f666617f837e8
Rozova <mrozova@imac-fk.harryfox.com>
2016-12-29 12:24:57 -0500

    Make command more dynamic by allowing user to change default greeting

--- src/SayHelloCommand.php
+++ src/SayHelloCommand.php
@@ -3,6 +3,7 @@
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SayHelloCommand extends Command{
@@ -11,12 +12,13 @@ class SayHelloCommand extends Command{
 	{
 		$this->setName('sayHelloTo')
 			->setDescription('Offer a greeting to the user.')
-			->addArgument('name', InputArgument::REQUIRED, 'Your name');
+			->addArgument('name', InputArgument::REQUIRED, 'Your name')
+			->addOption('greeting', null, InputOption::VALUE_OPTIONAL, 'Override the default greeting', 'Hello');
 	}
 
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
-		$message = 'Hello, ' . $input->getArgument('name');
+		$message = sprintf('%s, %s', $input->getOption('greeting'), $input->getArgument('name'));
 
 		$output->writeln("<comment>$message</comment>");
 	}
